58b92f62718342718d9b35a3f2b4a9d734d7f687
Russ211514 <russianjelomaster@gmail.com>
2026-01-28 23:10:11 +0800

    nigga

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -3,35 +3,15 @@ extends Container
 
 signal button_focused(button: BaseButton)
 signal button_pressed(button: BaseButton)
+signal ability_selected(ability: String)
 
 @export var auto_wrap: bool = true
-
 var index: int = 0
 
 func _ready() -> void:
-	
 	for button in get_buttons():
 		button.focused_entered.connect(on_button_focused.bind(button))
 		button.pressed.connect(on_button_pressed.bind(button))
-	
-	# TODO Fix for grids (poss only issue with > 2 col grid)
-	if !auto_wrap:
-		return
-	
-	var _class: String = get_class()
-	var buttons: Array = get_buttons()
-	var use_this_on_grid_containers: bool = false
-	
-	if use_this_on_grid_containers and get("columns"):
-		var top_row: Array = []
-		var bottom_row: Array = []
-		var cols: int = self.columns
-		var rows: int = round(buttons.size() / cols)
-		var btm_range: Array = [rows * cols - cols, rows * cols - 1]
-
-	# Ability Menu for Combat
-
-signal ability_selected(ability: String)
 
 func get_buttons() -> Array:
 	return get_children()
@@ -41,10 +21,8 @@ func button_focus(n: int = index) -> void:
 	button.grab_focus()
 
 func on_button_focused(button: BaseButton) -> void:
-		   # Optionally highlight or show info about the ability
-	pass
+	emit_signal("button_focused", button)
 
 func on_button_pressed(button: BaseButton) -> void:
-		   # Emit a signal with the selected ability name (button text)
+	emit_signal("button_pressed", button)
 	emit_signal("ability_selected", button.text)
-		# Repeat for left and right columns.
